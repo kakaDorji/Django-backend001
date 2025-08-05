9ed27af53d52844ba85a841b0a1e5c39fa54d794
karson <karsonam458@gmail.com>
2025-08-05 19:06:53 +0600

    Initial commit

---
+++ contacts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets           
+from .serializers import ContactSerializer
+from .models import Contact
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
