fd02edd27521e52ac453da72ba36a3296e13f355
karson <karsonam458@gmail.com>
2025-08-05 21:10:56 +0600

    allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&8m18rhnplt@_cq%)pcfcq=*3+vsax(jg7zztblte%zd%w0o@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-backend001.onrender.com']
 
 
 # Application definition
